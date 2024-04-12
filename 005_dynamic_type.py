a8ce23628b402bf140c957b929349c209a9a4084
Jittagorn Pitakmetagoon <jittagornp@gmail.com>
2024-04-12 20:08:51 +0700

    feat: Add dynamic type example

---
+++ 005_dynamic_type.py
@@ -0,0 +1,24 @@
+a = 1
+
+print("a:", a)
+
+a = 1.1
+
+print("a:", a)
+
+a = "Hello"
+
+print("a:", a)
+
+a = True
+
+print("a:", a)
+
+'''
+This is output result
+
+a: 1
+a: 1.1
+a: Hello
+a: True
+'''
